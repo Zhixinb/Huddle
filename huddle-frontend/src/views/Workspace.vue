d85c2d1a4d056f33d37d197f18e5ee6cbc54fd16
Frankie Wang <frankiewang19@gmail.com>
2020-11-12 23:19:32 -0500

    Added basic textbox and drag

--- huddle-frontend/src/views/Workspace.vue
+++ huddle-frontend/src/views/Workspace.vue
@@ -14,8 +14,7 @@
         <v-spacer></v-spacer>
         <v-tooltip bottom>
              <template v-slot:activator="{ on, attrs }">
-                <v-btn icon v-bind="attrs" v-on="on" id="text_box">
-                    <!-- <v-icon>mdi-vector-square</v-icon> -->
+                <v-btn icon v-bind="attrs" v-on="on" id="textbox" draggable @dragstart="onDragStart($event)" @dragend="onDragEnd()">
                 </v-btn>
              </template>
              <span>New Textbox</span>
@@ -28,12 +27,16 @@
              </template>
              <span>New Slide</span>
         </v-tooltip>
+        <button type="button" @click="toggle" >Fullscreen</button>
     </v-app-bar>
 
     <div class="pa-5">
         <v-fade-transition appear>
-            <v-card width=125vh height=70.3125vh>
-                <div id="graph-wrapper"></div>
+            <v-card id ="graph-wrapper" :width="w*0.7" :height="h*0.7" @dragover="onHover($event)" 
+            @drop="onDrop($event)">
+                <fullscreen ref="fullscreen" @change="fullscreenChange" background=#FFF>
+                    <textbox v-if="dragged === 'textbox'" :width="w*0.7" :height="h*0.7" :x="x" :y="y" />
+                </fullscreen>
             </v-card>
         </v-fade-transition>
     </div>
@@ -44,124 +47,73 @@
 <script>
 import Slide from '@/components/app/Slide';
 import {mapState} from 'vuex';
-
-var graph;
+import fullscreen from 'vue-fullscreen';
+import Vue from 'vue';
+import Textbox from '../components/app/Textbox.vue';
+Vue.use(fullscreen);
 
 export default {
     name: 'Workspace',
-    data() {
-        return {
-            curr_slide_id: 0,
-            slides: [
-                { id: 0, message: '0' }
-            ]
-        }
+    data: () => ({
+        curr_slide_id: 0,
+        slides: [
+            { id: 0, message: '0' }
+        ],
+        fullscreen: false,
+
+        // Dragging elements state
+        dragged: '',
+        x: 0,
+        y: 0,
+        w: window.innerWidth,
+        h: window.innerHeight
+    }),
+    created() {
+        window.addEventListener("resize", this.screenChange);
     },
-    mounted() {
-        this.draw();
+    destroyed() {
+        window.removeEventListener("resize", this.screenChange);
     },
     methods:
     {
         update_slide(id) {
-            this.load_slide();
-            graph.getModel().clear();
             this.curr_slide_id = id;
-            var xml = this.slides[this.curr_slide_id].message;
-            var doc = mxUtils.parseXml(xml);
-            var codec = new mxCodec(doc);
-            codec.decode(doc.documentElement, graph.getModel());
-            var elt = doc.documentElement.firstChild;
-            var cells = [];
-
-            while (elt != null) {
-                cells.push(codec.decode(elt));
-                elt = elt.nextSibling;
-            }
-
-            graph.addCells(cells);
-        },
-        load_slide() {
-            var encoder = new mxCodec();
-            var result = encoder.encode(graph.getModel());
-            var xml = mxUtils.getXml(result);
-            this.slides[this.curr_slide_id].message = xml;
         },
         append_slide() {
-            this.load_slide();
             this.slides.push({ id: this.slides.length, message: this.slides.length });
         },
-        draw()
+        toggle() 
         {
-            if (!mxClient.isBrowserSupported()) {
-                mxUtils.error("Browser is not supported!", 200, false);
-            } else {
-                // new toolbar without event processing
-                var toolbar = new mxToolbar(document.getElementById("text_box"));
-                toolbar.enabled = false;
-
-                // the div for the graph
-                var container = document.createElement("div");   
-                container.id = "grid" // KS: add an Id so we can find it later
-                container.style.position = "absolute";
-                container.style.overflow = "hidden";
-                container.style.left = "0px";
-                container.style.top = "0px";
-                container.style.right = "0px";
-                container.style.bottom = "0px";
-                container.style.background =
-                'url("https://jgraph.github.io/mxgraph/javascript/examples/editors/images/grid.gif")';
-
-                document.getElementById("graph-wrapper").appendChild(container);
-
-                // creates the model and the graph inside the container
-                var model = new mxGraphModel();
-                graph = new mxGraph(container, model);
-
-                graph.setConnectable(false);
-                graph.setMultigraph(false);
-
-                var addVertex = function(icon, w, h, style) {
-                    var vertex = new mxCell(null, new mxGeometry(0, 0, w, h), style);
-                    vertex.setVertex(true);
-
-                    var funct = function(graph, evt, cell, x, y) {
-                        graph.stopEditing(false);
-
-                        var v = graph.getModel().cloneCell(vertex);
-                        v.geometry.x = x;
-                        v.geometry.y = y;
-
-                        graph.addCell(v);
-                        graph.setSelectionCell(v);
-                    };
-
-                    // Creates the image which is used as the drag icon (preview)
-                    var img = toolbar.addMode(null, icon, function(evt, cell) {
-                        var pt = this.graph.getPointForEvent(evt);
-                        funct(graph, evt, cell, pt.x, pt.y);
-                    });
-
-                    mxEvent.addListener(img, "mousedown", function(evt) {
-                        if (img.enabled == false) {
-                        mxEvent.consume(evt);
-                        }
-                    });
-
-                    mxUtils.makeDraggable(img, graph, funct);
-
-                    graph.getSelectionModel().addListener(mxEvent.CHANGE, function() {
-                        var tmp = graph.isSelectionEmpty();
-                        mxUtils.setOpacity(img, tmp ? 100 : 20);
-                        img.enabled = tmp;
-                    });
-                };
-
-                addVertex("https://jgraph.github.io/mxgraph/javascript/examples/editors/images/rounded.gif", 100, 40, "shape=rounded");
+            this.$refs['fullscreen'].toggle()
+        },
+        fullscreenChange(fullscreen) {
+            this.fullscreen = fullscreen
+        },
+        onDragStart(event) {
+            event.dataTransfer.setDragImage(document.createElement('div'), 0, 0);
+            this.dragged = event.target.id;
+        },
+        onDragEnd() {
+            this.dragged = '';
+        },
+        onHover(event) {
+            event.preventDefault();
+            const pos = document.getElementById('graph-wrapper').getBoundingClientRect();
+            if (event.x <= pos.right && event.y <= pos.bottom && event.x >= pos.left && event.y >= pos.top) {
+                this.x = event.x - pos.x;
+                this.y = event.y - pos.y;
             }
+        },
+        onDrop (event) {
+        },
+        screenChange(event) {
+            this.w = window.innerWidth;
+            this.h = window.innerHeight;
         }
     },
     components: {
-        Slide
+        Slide,
+        Textbox
     },
     computed: {
         ...mapState(['workspace'])
