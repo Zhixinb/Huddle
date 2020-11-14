4138b40d812a7f52c696b1088f8af70952893fc8
Zhixin Zhang <r914721222@gmail.com>
2020-11-14 15:12:37 -0500

    WIP dynamic component creation and update

--- huddle-frontend/src/views/Workspace.vue
+++ huddle-frontend/src/views/Workspace.vue
@@ -3,6 +3,11 @@
     <v-card>
       <div>
           <h3>Workspace ID : {{workspace}}</h3>
+          <button type="button" v-on:click="addFormElement('textbox')">Add Textbox</button>
+          <button type="button" v-on:click="clear()">Clear Textbox</button>
+      </div>
+      <div>
+        <component v-for="field in fields" v-bind:is="field.type" :key="field.id"></component>
       </div>
     </v-card>
   </v-fade-transition>
@@ -11,11 +16,30 @@
 <script>
 
 import {mapState} from 'vuex'
+import textbox from '@/components/widgets/textbox'
 
 export default {
   name: 'Workspace',
+  components: {
+    textbox
+  },
+  data: () => ({
+    fields: [],
+    count: 0
+  }),
   computed: {
     ...mapState(['workspace'])
+  },
+  methods: {
+    addFormElement (type) {
+      this.fields.push({
+        'type': type,
+        id: this.count++
+      })
+    },
+    clear () {
+      this.components[0].data.text_content = 'qqqqqqq'
+    }
   }
 }
 </script>
