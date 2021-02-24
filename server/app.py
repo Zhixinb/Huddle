0cf6aed48a77bde5191ff98a7bd74600b0fccddf
Zhixin Zhang <r914721222@gmail.com>
2021-02-23 19:56:38 -0500

    Cleaned up sevrer CORS origin list

--- server/app.py
+++ server/app.py
@@ -14,15 +14,7 @@ app.config['SECRET_KEY'] = 'secret!'
 # use talisman for SSL if in prod
 talisman = None if os.environ.get('IS_HEROKU', False) else  Talisman(app)
 
-socketio = SocketIO(app, cors_allowed_origins=["http://localhost:8080", "https://amplifyapp.com:*", 
-                                               "https://herokuapp.com:*", "https://cors-everywhere-me.herokuapp.com:*", 
-                                               "https://robin-dev.d1jfi0qjq3gsdb.amplifyapp.com:*", "https://www.amplifyapp.com:*", 
-                                               "https://www.herokuapp.com:*", "https://www.cors-everywhere-me.herokuapp.com:*", 
-                                               "https://www.robin-dev.d1jfi0qjq3gsdb.amplifyapp.com:*", "https://amplifyapp.com", 
-                                               "https://herokuapp.com", "https://cors-everywhere-me.herokuapp.com", 
-                                               "https://robin-dev.d1jfi0qjq3gsdb.amplifyapp.com", "https://www.amplifyapp.com", 
-                                               "https://www.herokuapp.com", "https://www.cors-everywhere-me.herokuapp.com", 
-                                               "https://www.robin-dev.d1jfi0qjq3gsdb.amplifyapp.com"])
+socketio = SocketIO(app, cors_allowed_origins=["http://localhost:8080", "https://robin-dev.d1jfi0qjq3gsdb.amplifyapp.com"])
 ROOMS = {}  # dict to track active workspaces
 ROUTERS = {}  # dict to track routers
 
